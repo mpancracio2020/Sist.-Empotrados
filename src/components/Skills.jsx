7896b968f776dc1e259105ba3998404eaafea8c9
Marvin <90764466+mpancracio2020@users.noreply.github.com>
2023-09-18 11:59:23 +0200

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,6 +1,6 @@
 import { useAppContext } from "../appContext";
 import { Element } from "react-scroll";
-import React, { Component }  from 'react';
+import * as React from 'react';
 // Data
 import { skillData, resume } from "../data";
 // Components
