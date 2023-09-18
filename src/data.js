8e32953d79e62c5cc45c90a4c480de14c36344b1
Marvin <90764466+mpancracio2020@users.noreply.github.com>
2023-09-18 15:43:22 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -79,12 +79,12 @@ export const resume = null;
  ************************************************************** 
   List the repo names (string - "your-repo-name") you want to include (they will be sorted alphabetically). If empty, only the first 3 will be included.
 */
-export const filteredProjects = ["example-1", "example-2", "example-3"];
+export const filteredProjects = ["practice 1"];
 
 // Replace the defualt GitHub image for matching repos below (images imported above - lines 7-8)
 export const projectCardImages = [
   {
-    name: "example-1",
+    name: "practice 1",
     image: Logo,
   },
 ];
