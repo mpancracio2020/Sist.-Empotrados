3cb2c1ba9a1df27462876a0deb5860a142383353
Marvin <90764466+mpancracio2020@users.noreply.github.com>
2023-09-27 19:39:58 +0200

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -208,7 +208,7 @@ export default function Projects() {
 
              
           <iframe height="480" width="500"
-           src="https://youtu.be/xwCpgUhGOrc">
+           src="https://youtu.be/xwCpgUhGOrc?rel=0">
           </iframe>
                     
           {/*mainProjects.length !== 0 && (
