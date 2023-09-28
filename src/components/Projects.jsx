b4d962bc64df5528e680233d6d22f11c584aa28e
Marvin <90764466+mpancracio2020@users.noreply.github.com>
2023-09-28 13:21:08 +0200

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -205,9 +205,11 @@ export default function Projects() {
 
       
             Here is an example video: {"\n"} {"\n"}
-           <iframe width="560" height="315" src="https://www.youtube.com/embed/xwCpgUhGOrc?si=8J8rmt9WVKy6X2y-" 
+           <div>
+             <iframe width="560" height="315" src="https://www.youtube.com/embed/xwCpgUhGOrc?si=8J8rmt9WVKy6X2y-" 
              title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
              allowfullscreen></iframe>  
+             </div>
           {/*mainProjects.length !== 0 && (
             <>
               <Row xs={1} md={2} lg={3} className="g-4 justify-content-center">
