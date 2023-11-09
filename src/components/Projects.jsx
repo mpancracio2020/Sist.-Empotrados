7868a752509ad26f629ead9540161c71ee847477
Marvin <90764466+mpancracio2020@users.noreply.github.com>
2023-11-09 19:21:15 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -98,7 +98,7 @@ export default function Projects() {
                     to be able to recognize it,therefore, we implemented an algorithm that allows us to detect faces with any rotation
                     <center>
                     
-                    <img src={obama}
+                    <img align="right"  src={obama}
                        style={{ width: '200px', height: 'auto' }}
                       />
   
