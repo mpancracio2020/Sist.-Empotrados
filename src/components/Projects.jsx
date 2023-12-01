e79f85950419b9ebe41e50a81608d8ce10c33357
Marvin <90764466+mpancracio2020@users.noreply.github.com>
2023-12-01 11:58:46 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -114,11 +114,12 @@ export default function Projects() {
                                   } />
               ))}
             </div>
-          </div>
-           <Title>
+            <Title>
               <h2>Projects</h2>
               <div className="underline"></div>
             </Title>
+          </div>
+           
            
           {/* P2 -END*/}
           
